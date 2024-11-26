c38c5f70cde9861a760a542e108bc8dd3ac2facd
ravi <m.ravi0527@gmail.com>
2024-11-26 19:26:31 +0530

    Setup Project and tailwind and auth0

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./Components/Login/LoginButton";
+import LogoutButton from "./Components/Login/LogoutButton";
 
 function App() {
+  const { user, isAuthenticated } = useAuth0();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto ">
+      <LoginButton />
+      <LogoutButton />
+      {isAuthenticated && (
+        <div>
+          <img src={user?.picture} alt={user?.name} />
+          <h2>{user?.name}</h2>
+          <p>{user?.email}</p>
+        </div>
+      )}
     </div>
   );
 }
